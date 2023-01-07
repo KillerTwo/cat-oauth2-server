c427c38e2363e585b965d3263aea3dd5369f27ba
KillerTwo <lwt2710@163.com>
2023-01-07 16:41:19 +0800

    替换jwt实现方式为nimbus-jose-jwt

--- backend/boogle-system/src/main/java/org/wm/authentication/filter/JwtAuthenticationTokenFilter.java
+++ backend/boogle-system/src/main/java/org/wm/authentication/filter/JwtAuthenticationTokenFilter.java
@@ -39,13 +39,13 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-        /*LoginUser loginUser = tokenService.getLoginUser(request);
+        LoginUser loginUser = tokenService.getLoginUser(request);
         if (StringUtils.isNotNull(loginUser) && StringUtils.isNull(SecurityUtils.getAuthentication())) {
             tokenService.verifyToken(loginUser);
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        }*/
+        }
         chain.doFilter(request, response);
     }
 }
